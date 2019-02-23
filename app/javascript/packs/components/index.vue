33c0c8b48b9d102e3cc88c1516d1d94734def78d
島袋雄太 <mrs1122ani@gmail.com>
2019-02-23 17:49:12 +0900

    add task

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -4,10 +4,10 @@
             <!-- 新規作成部分 -->
             <div class="row">
                 <div class="col s10 m11">
-                    <input class="form-control" placeholder="Add your task!!">
+                    <input v-model="newTask" class="form-control" placeholder="Add your task!!">
                 </div>
                 <div class="col s2 m1">
-                    <div class="btn-floating waves-effect waves-light red">
+                    <div v-on:click="createTask" class="btn-floating waves-effect waves-light red">
                         <i class="material-icons">add</i>
                     </div>
                 </div>
@@ -17,7 +17,7 @@
                 <ul class="collection">
                     <li v-for="task in tasks" v-if="!task.is_done" v-bind:id="'row_task_' + task.id"
                         class="collection-item">
-                        <input type="checkbox" v-bind:id="'task_' + task.id"/>
+                        <input type="checkbox" v-on:change="doneTask(task.id)" v-bind:id="'task_' + task.id"/>
                         <label v-bind:for="'task_' + task.id">{{ task.name }}</label>
                     </li>
                 </ul>
@@ -62,9 +62,19 @@
                     console.log(error);
                 });
             },
-               displayFinishedTasks: function() {
-             document.querySelector('#finished-tasks').classList.toggle('display_none');
-           },
+            displayFinishedTasks: function () {
+                document.querySelector('#finished-tasks').classList.toggle('display_none');
+            },
+            createTask: function () {
+                if (!this.newTask) return;
+
+                axios.post('/api/tasks', {task: {name: this.newTask}}).then((response) => {
+                    this.tasks.unshift(response.data.task);
+                    this.newTask = '';
+                }, (error) => {
+                    console.log(error);
+                });
+            }
         }
     }
 
